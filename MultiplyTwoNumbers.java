a58abd0c8cf695e97d1e2c041386ee69fa9a514d
Prem Bhalerao <114722173+premsbhalerao@users.noreply.github.com>
2024-01-08 10:34:53 +0530

    file adding

---
+++ MultiplyTwoNumbers.java
@@ -0,0 +1,8 @@
+public class MultiplyTwoNumbers{
+    public static void main(String[] args){
+        float first = 3.5f;
+        float second = 2.4f;
+        float product = first * second;
+        System.out.println("the product is: "+ product);
+    }
+}
